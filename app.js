99137257f005d98d311ff64a90003105eb6c7bf5
TJ Holowaychuk <tj@vision-media.ca>
2011-12-18 00:01:53 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(3000);
+
+console.log("Express server listening for connections");
