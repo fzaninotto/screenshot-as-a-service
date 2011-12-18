91648601edd3502da5fc3481d48c51d69496553d
TJ Holowaychuk <tj@vision-media.ca>
2011-12-18 01:06:57 -0800

    nicer api

--- routes.js
+++ routes.js
@@ -14,7 +14,6 @@ var dir = app.get('screenshots')
  */
 
 app.get('/', function(req, res, next){
-  if (req.query.url) return next();
   res.render('index', { title: 'Express' });
 });
 
@@ -22,8 +21,8 @@ app.get('/', function(req, res, next){
  * GET screenshot.
  */
 
-app.get('/', ratelimit(100, 10), function(req, res, next){
-  var url = req.query.url;
+app.get('/:url(*)', ratelimit(100, 10), function(req, res, next){
+  var url = req.params.url;
   if (!url) return res.send(400);
   var id = utils.md5(url);
   var path = join(dir, id + '.png');
