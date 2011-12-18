b836af7fceb772841c278fb79d1884a0c6edf818
TJ Holowaychuk <tj@vision-media.ca>
2011-12-18 13:16:15 -0800

    setting up templates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,5 +6,20 @@ o = $;
 // boot
 
 o(function(){
-  console.log('whoop');
+  console.log(render('screenshot', { title: 'yay' }));
 })
+
+function render(name, obj) {
+  var tmpl = template(name);
+  return whiskers.render(tmpl, obj);
+}
+
+/**
+ * Grab template `name`.
+ */
+
+function template(name) {
+  return document
+    .getElementById(name + '-template')
+    .innerHTML;
+}
