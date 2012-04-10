52c5bb513fbc8e485bf3069126dc5cb4908b5aa4
Francois Zaninotto <fzaninotto@gmail.com>
2012-04-10 22:58:23 +0200

    Make rasterizer server work

--- scripts/rasterizer.js
+++ scripts/rasterizer.js
@@ -17,7 +17,6 @@ defaultViewportSize = {
   height: ~~defaultViewportSize[1] || 600
 };
 
-var page = new WebPage();
 var pageSettings = ['javascriptEnabled', 'loadImages', 'localToRemoteUrlAccessEnabled', 'userAgent', 'userName', 'password'];
 
 var server, service;
@@ -25,53 +24,70 @@ var server, service;
 server = require('webserver').create();
 
 /*
- * Screenshot route
+ * Screenshot service
  *
  * Generate a screenshot file on the server under the basePath
  *
  * Usage:
- * GET /http%3A%2F%2Fwww.google.com
- * path: google.png
- * 
+ * GET /
+ * url: http://www.google.com
+ *
  * Optional headers:
+ * path: google.png
  * width: 1024
  * height: 600
+ * clipRect: { "top": 14, "left": 3, "width": 400, "height": 300 }
  *
- * All settings of the WebPage object can also be set using headers, e.g.
+ * If path is omitted, the service creates it based on the url, removing the
+ * protocol and replacing all slashes with dots, e.g
+ * http://www.google.com => www.google.com.png
+ *
+ * width and height represent the viewport size. If the content exceeds these
+ * boundaries and has a non-elastic style, the screenshot may have greater size.
+ * Use clipRect to ensure the final size of the screenshot in pixels.
+ *
+ * All settings of the WebPage object can also be set using headers, e.g.:
  * javascriptEnabled: false
  * userAgent: Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+
- *
- * Note that the URI to be polled must be url-encoded using the encodeURIComponent() function
  */ 
 service = server.listen(port, function(request, response) {
-
-  var url = urlDecode(request.url.substr(1));
-  var path = basePath + (request.headers.path || (url.replace(new RegExp('https?:/'), '').replace('/', '.') + '.png'));
-
+  if (!request.headers.url) {
+    response.statusCode = 400;
+    response.write('Error: Request must contain an url header' + "\n");
+    response.close();
+    return;
+  }
+  var url = request.headers.url;
+  var path = basePath + (request.headers.path || (url.replace(new RegExp('https?://'), '').replace(/\//g, '.') + '.png'));
+  var page = new WebPage();
   page.viewportSize = {
-  	width: request.headers.width || defaultViewportSize.width,
-  	height: request.headers.height || defaultViewportSize.height
+    width: request.headers.viewportWidth || defaultViewportSize.width,
+    height: request.headers.viewportHeight || defaultViewportSize.height
+  };
+  if (request.headers.clipRect) {
+    page.clipRect = JSON.parse(request.headers.clipRect);
   };
   for (name in pageSettings) {
-    if (value = request.headers[name]) {
+    if (value = request.headers[pageSettings[name]]) {
       value = (value == 'false') ? false : ((value == 'true') ? true : value);
-      page.settings[name] = value;
+      page.settings[pageSettings[name]] = value;
     }
   };
   page.open(url, function(status) {
     if (status == 'success') {
       page.render(path);
-      page.release();
-      response.statusCode = 200;
-      response.close();
+      response.write('Success: Screenshot saved to ' + path + "\n");
     } else {
-      response.statusCode = 404;
-      response.write('Failed to load ' + url);
-      response.close();
+      response.write('Error: Url returned status ' + status + "\n");
     }
+    page.release();
+    response.close();
   });
+  page.onError = function(msg, trace) {
+    response.write('Error: Page triggered ' + msg + "\n");
+    response.close();
+  };
+  // must start the response now, or phantom closes the connection
+  response.statusCode = 200;
+  response.write('');
 });
-
-function urlDecode(str) {
-   return decodeURIComponent((str + '').replace(/\+/g, '%20'));
-}
