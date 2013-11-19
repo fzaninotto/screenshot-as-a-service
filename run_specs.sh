d8a42195c07e1a340e90f67a84bd79955f388a52
Oliver Spindler <os@oliverspindler.com>
2013-11-19 10:42:24 +0000

    Propagate test exit code

--- run_specs.sh
+++ run_specs.sh
@@ -3,5 +3,7 @@ node app.js & SERVICE=$!
 node spec/support/test_server.js & CLIENT=$!
 sleep 4
 node_modules/jasmine-node/bin/jasmine-node spec/
+EXIT_CODE=$?
 kill $SERVICE
 kill $CLIENT
+exit $EXIT_CODE
